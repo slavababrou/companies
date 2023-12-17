07d0e44d0e16cf87c2d844e2176e218733067932
Vlad <z0l0t0m@mail.ru>
2023-12-18 02:10:47 +0300

    Bugs

--- src/components/CompanyRevievItem/CompanyRevievItem.jsx
+++ src/components/CompanyRevievItem/CompanyRevievItem.jsx
@@ -2,8 +2,23 @@ import React from "react";
 import styles from "./CompanyRevievItem.module.css";
 import logo from "../../images/logo/logo.svg";
 import TimeAgo from "../TimeAgo/TimeAgo";
+import { useDispatch } from "react-redux";
+import { addLikeDislikeReviev } from "../../store/companySlice";
 
 const ReviewItem = ({ reviev, user }) => {
+  const dispatch = useDispatch();
+
+  const addLike = async (revievId) => {
+    const actionType = "like";
+    const userId = user?.id;
+    await dispatch(addLikeDislikeReviev({ revievId, actionType, userId }));
+  };
+  const addDislike = async (revievId) => {
+    const actionType = "dislike";
+    const userId = user?.id;
+    await dispatch(addLikeDislikeReviev({ revievId, actionType, userId }));
+  };
+
   return (
     <div className={styles["list_item"]}>
       <div className={styles["list_item_logo-wrapper"]}>
@@ -16,6 +31,17 @@ const ReviewItem = ({ reviev, user }) => {
         <span>{user?.login}</span>
       </div>
       <span className={styles["list_item_text"]}>{reviev.text}</span>
+      <div className={styles["list_item_likes"]}>
+        <button className={styles["likes"]} onClick={() => addLike(reviev.id)}>
+          Лайки: {reviev.likes}
+        </button>
+        <button
+          className={styles["dislikes"]}
+          onClick={() => addDislike(reviev.id)}
+        >
+          Дизлайки: {reviev.dislikes}
+        </button>
+      </div>
     </div>
   );
 };
--- src/store/companySlice.js
+++ src/store/companySlice.js
@@ -98,6 +98,30 @@ export const fetchSameCompaniesData = createAsyncThunk(
   }
 );
 
+export const addLikeDislikeReviev = createAsyncThunk(
+  "company/likeDislikeReviev",
+  async ({ revievId, actionType, userId }, { getState }) => {
+    try {
+      const userId = getState().auth.user?.user?.id;
+
+      if (
+        actionType === "like" ||
+        (actionType === "dislike" && userId && revievId)
+      ) {
+        const response = await axios.post(
+          `http://localhost:3192/api/reviev/add-like-dislike`,
+          { id: revievId, actionType, userId }
+        );
+
+        return response.data;
+      }
+    } catch (error) {
+      console.error("Error liking/disliking reviev:", error);
+      throw error;
+    }
+  }
+);
+
 const companySlice = createSlice({
   name: "company",
   initialState: {
@@ -160,9 +184,61 @@ const companySlice = createSlice({
       })
       .addCase(createCompany.rejected, (state) => {
         state.isLoading = false;
+      })
+      .addCase(addLikeDislikeReviev.fulfilled, async (state, action) => {
+        const { actionType, userId } = action.payload[0][0][0];
+        const revievId = action.payload[0][0][0]?.id;
+
+        const revievsDataCopy = JSON.parse(JSON.stringify(state.revievsData));
+
+        const reviev = revievsDataCopy?.rows.find((rev) => rev.id === revievId);
+
+        if (!revievId || !userId) return;
+        const likedDisliked = await getLikedDislikedState(revievId, userId);
+
+        if (reviev) {
+          if (actionType === "like" && !likedDisliked) {
+            reviev.likes += 1;
+            if (likedDisliked === "liked") {
+              reviev.dislikes -= 1; // Убираем дизлайк, если он был
+            }
+          } else if (actionType === "dislike" && !likedDisliked) {
+            reviev.dislikes += 1;
+            if (likedDisliked === "liked") {
+              reviev.likes -= 1; // Убираем лайк, если он был
+            }
+          }
+        }
+
+        state.revievsData = revievsDataCopy;
       });
   },
 });
+
+async function getLikedDislikedState(revievId, userId) {
+  try {
+    console.log(userId, revievId);
+    const response = await axios.post(
+      `http://localhost:3192/api/like/check-user-liked-disliked`,
+      { userId: userId, revievId: revievId }
+    );
+
+    const alreadyLikedOrDisliked = response.data?.alreadyLikedOrDisliked;
+    const actionType = response.data?.actionType;
+
+    if (actionType === "like" && alreadyLikedOrDisliked) {
+      return "liked";
+    } else if (actionType === "dislike" && alreadyLikedOrDisliked) {
+      return "disliked";
+    }
+
+    return null;
+  } catch (error) {
+    console.error("Error checking user liked or disliked:", error);
+    throw error;
+  }
+}
+
 export const selectRevievsDataFromStore = (state) => {
   return state?.company?.revievsData;
 };
